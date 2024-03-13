532f90107104afd03c856e1b2b4a53064fb2f3db
souleyy <trabelsysouley.isamm.tn@gmail.com>
2024-03-13 01:12:30 +0100

    add  logout into backoffice

--- resources/views/Admin/Home/index.blade.php
+++ resources/views/Admin/Home/index.blade.php
@@ -47,6 +47,13 @@
       <li class="nav-item d-none d-sm-inline-block">
         <a href="#" class="nav-link">Contact</a>
       </li>
+      <li class="nav-item d-none d-sm-inline-block">
+        <a  href="{{ route('logout') }}" onclick="event.preventDefault();
+                                                     document.getElementById('logout-form').submit();" class="nav-link">Déconnecter</a>
+       <form id="logout-form" action="{{ route('logout') }}" method="POST" class="d-none">
+                                        @csrf
+                                    </form>
+    </li>
     </ul>
 
     <!-- Right navbar links -->
