cab80424fdc1b435b21bcb8d725358ad3b79aac4
Rabiezouita11 <rabiezouita82@gmail.com>
2024-05-27 01:21:09 +0100

    update

--- resources/views/Client/Home/index.blade.php
+++ resources/views/Client/Home/index.blade.php
@@ -674,70 +674,7 @@
         <!-- blog-area-end -->
 
         <!-- feature-area-start -->
-        <section class="feature-area mainfeature__bg grey-bg pt-50 pb-40"
-            data-background="assets/img/shape/footer-shape-1.svg">
-            <div class="container">
-                <div class="mainfeature__border pb-15">
-                    <div class="row row-cols-lg-5 row-cols-md-3 row-cols-2">
-                        <div class="col">
-                            <div class="mainfeature__item text-center mb-30">
-                                <div class="mainfeature__icon">
-                                    <img src="assets/img/icon/feature-icon-1.svg" alt="">
-                                </div>
-                                <div class="mainfeature__content">
-                                    <h4 class="mainfeature__title">Fast Delivery</h4>
-                                    <p>Across West & East India</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col">
-                            <div class="mainfeature__item text-center mb-30">
-                                <div class="mainfeature__icon">
-                                    <img src="assets/img/icon/feature-icon-2.svg" alt="">
-                                </div>
-                                <div class="mainfeature__content">
-                                    <h4 class="mainfeature__title">safe payment</h4>
-                                    <p>100% Secure Payment</p>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col">
-                            <div class="mainfeature__item text-center mb-30">
-                                <div class="mainfeature__icon">
-                                    <img src="assets/img/icon/feature-icon-3.svg" alt="">
-                                </div>
-                                <div class="mainfeature__content">
-                                    <h4 class="mainfeature__title">Online Discount</h4>
-                                    <p>Add Multi-buy Discount </p>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col">
-                            <div class="mainfeature__item text-center mb-30">
-                                <div class="mainfeature__icon">
-                                    <img src="assets/img/icon/feature-icon-4.svg" alt="">
-                                </div>
-                                <div class="mainfeature__content">
-                                    <h4 class="mainfeature__title">Help Center</h4>
-                                    <p>Dedicated 24/7 Support </p>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col">
-                            <div class="mainfeature__item text-center mb-30">
-                                <div class="mainfeature__icon">
-                                    <img src="assets/img/icon/feature-icon-5.svg" alt="">
-                                </div>
-                                <div class="mainfeature__content">
-                                    <h4 class="mainfeature__title">Curated items</h4>
-                                    <p>From Handpicked Sellers</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-        </section>
+     
         <!-- feature-area-end -->
 
     </main>
