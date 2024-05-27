845b55b0e38e778b5dd6469f3d83645e63be812a
Rabiezouita11 <rabiezouita82@gmail.com>
2024-05-27 01:14:48 +0100

    update

--- resources/views/Client/Home/index.blade.php
+++ resources/views/Client/Home/index.blade.php
@@ -23,6 +23,9 @@
     <link rel="stylesheet" href="assets/css/fontawesome.min.css">
     <link rel="stylesheet" href="assets/css/icon-dukamarket.css">
     <link rel="stylesheet" href="assets/css/main.css">
+
+    <link rel="stylesheet" href="fonts/font-awesome/css/font-awesome.min.css" type="text/css">
+<link rel="stylesheet" type="text/css" href="fonts/Simple-Line-Icons-Webfont/simple-line-icons.css" media="screen" />
 </head>
 
 <body>
@@ -728,97 +731,26 @@
 
     <!-- footer-area-start -->
     <footer>
-        <div class="tpfooter__area theme-bg-2">
-            <div class="tpfooter__top pb-15">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-xl-3 col-lg-4 col-md-6 col-sm-6">
-                            <div class="tpfooter__widget footer-col-1 mb-50">
-                                <h4 class="tpfooter__widget-title">Let Us Help You</h4>
-                                <p>If you have any question, please <br> contact us at:
-                                    <a href="mailto:support@example.com">support@example.com</a>
-                                </p>
-                                <div class="tpfooter__widget-social mt-45">
-                                    <span class="tpfooter__widget-social-title mb-5">Social Media:</span>
-                                    <a href="#"><i class="fab fa-facebook-f"></i></a>
-                                    <a href="#"><i class="fab fa-twitter"></i></a>
-                                    <a href="#"><i class="fab fa-youtube"></i></a>
-                                    <a href="#"><i class="fab fa-pinterest-p"></i></a>
-                                    <a href="#"><i class="fab fa-skype"></i></a>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-xl-3 col-lg-4 col-md-6 col-sm-6">
-                            <div class="tpfooter__widget footer-col-2 mb-50">
-                                <h4 class="tpfooter__widget-title">Looking for Orfarm?</h4>
-                                <p>68 St. Vicent Place, Glasgow, Greater <br> Newyork NH2012, UK.</p>
-                                <div class="tpfooter__widget-time-info mt-35">
-                                    <span>Monday – Friday: <b>8:10 AM – 6:10 PM</b></span>
-                                    <span>Saturday: <b>10:10 AM – 06:10 PM</b></span>
-                                    <span>Sunday: <b>Close</b></span>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-xl-2 col-lg-4 col-md-4 col-sm-5">
-                            <div class="tpfooter__widget footer-col-3 mb-50">
-                                <h4 class="tpfooter__widget-title">HOT CATEGORIES</h4>
-                                <div class="tpfooter__widget-links">
-                                    <ul>
-                                        <li><a href="#">Fruits & Vegetables</a></li>
-                                        <li><a href="#">Dairy Products</a></li>
-                                        <li><a href="#">Package Foods</a></li>
-                                        <li><a href="#">Beverage</a></li>
-                                        <li><a href="#">Health & Wellness</a></li>
-                                    </ul>
-                                </div>
-                            </div>
-                        </div>
-                        <div class="col-xl-4 col-lg-6 col-md-8 col-sm-7">
-                            <div class="tpfooter__widget footer-col-4 mb-50">
-                                <h4 class="tpfooter__widget-title">Our newsletter</h4>
-                                <div class="tpfooter__widget-newsletter">
-                                    <p>Subscribe to the Orfarm mailing list to receive updates <br> on new arrivals &
-                                        other information.</p>
-                                    <form action="index.html">
-                                        <span><i><img src="assets/img/shape/message-1.svg"
-                                                    alt=""></i></span>
-                                        <input type="email" placeholder="Your email address...">
-                                        <button
-                                            class="tpfooter__widget-newsletter-submit tp-news-btn">Subscribe</button>
-                                    </form>
-                                    <div class="tpfooter__widget-newsletter-check mt-10">
-                                        <div class="form-check">
-                                            <input class="form-check-input" type="checkbox" value=""
-                                                id="flexCheckDefault">
-                                            <label class="form-check-label" for="flexCheckDefault">
-                                                I accept terms & conditions & privacy policy.
-                                            </label>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
+    <section class="section-less-padding-2">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-12 col-centered text-center"> <img src="images/logo/logo.png" alt=""/> </div>
+          <ul class="footer-social-icons-2">
+            <li><a target="_blank" class="twitter" href="https://twitter.com/codelayers"><i class="fa fa-twitter"></i></a></li>
+            <li><a target="_blank" href="https://www.facebook.com/codelayers"><i class="fa fa-facebook"></i></a></li>
+            <li><a href="#"><i class="fa fa-google-plus"></i></a></li>
+            <li><a href="#"><i class="fa fa-linkedin"></i></a></li>
+            <li><a href="#"><i class="fa fa-dribbble"></i></a></li>
+          </ul>
+          <br/>
+           <div class="col-md-12 col-centered text-center">
+              <p class="text-gray">Copyright © 2019 <br/>
+                Design & Developed by Codelayers</p>
             </div>
-            <div class="tpfooter___bottom pt-40 pb-40">
-                <div class="container">
-                    <div class="row">
-                        <div class="col-lg-6 col-md-7 col-sm-12">
-                            <div class="tpfooter__copyright">
-                                <span class="tpfooter__copyright-text">Copyright © <a href="#">ORFARM</a> all
-                                    rights reserved. Powered by <a href="#">ThemePure</a>.</span>
-                            </div>
-                        </div>
-                        <div class="col-lg-6 col-md-5 col-sm-12">
-                            <div class="tpfooter__copyright-thumb text-end">
-                                <img src="assets/img/shape/footer-payment.png " alt="">
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
+            
         </div>
+      </div>
+    </section>
     </footer>
     <!-- footer-area-end -->
 
